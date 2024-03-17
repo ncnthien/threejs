7f4741f8b2235ca291a2c734f7de42acdbb749cc
ncnthien <nhatthien185@gmail.com>
2024-03-17 11:15:14 +0700

    full screen and resizing

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 const canvas = document.querySelector('.webgl')
 const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
-// mesh.position.x = 0.7
-// mesh.position.y = -0.6
-// mesh.position.z = 1
-mesh.scale.x = 2
-mesh.scale.y = 0.25
-mesh.scale.z = 0.5
 
 scene.add(mesh)
 
@@ -18,14 +13,43 @@ const axesHelper = new THREE.AxesHelper(2)
 scene.add(axesHelper)
 
 const sizes = {
-  width: 800,
-  height: 600
+  width: window.innerWidth,
+  height: window.innerHeight
 }
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 1000)
 camera.position.z = 3
 scene.add(camera)
 
 const renderer = new THREE.WebGLRenderer({ canvas })
 renderer.setSize(sizes.width, sizes.height)
 
-renderer.render(scene, camera)
+const cursor = {
+  x: 0,
+  y: 0
+}
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableZoom = false
+controls.enableDamping = true
+
+window.addEventListener('mousemove', (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5
+  cursor.y = - (event.clientY / sizes.height - 0.5)
+})
+
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+  renderer.setSize(sizes.width, sizes.height)
+})
+
+function tick() {
+  camera.lookAt(mesh.position)
+  renderer.render(scene, camera)
+  controls.update()
+  window.requestAnimationFrame(tick)
+}
+
+tick()
