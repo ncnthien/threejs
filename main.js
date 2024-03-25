a5bfedde437e15d97179ddeae690fd774df98b9c
ncnthien <nhatthien185@gmail.com>
2024-03-25 18:01:29 +0700

    main: text gemetry

--- main.js
+++ main.js
@@ -1,73 +1,62 @@
 import * as THREE from 'three'
-import GUI from 'lil-gui'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js'
+import { FontLoader } from 'three/addons/loaders/FontLoader.js'
 
-const loadingManager = new THREE.LoadingManager()
-loadingManager.onStart = () => {
-  console.log('loading started')
-}
-loadingManager.onLoad = () => {
-  console.log('loading finished')
-}
-loadingManager.onProgress = () => {
-  console.log('loading progress')
-}
-loadingManager.onError = () => {
-  console.log('loading error')
-}
-
-const textureLoader = new THREE.TextureLoader(loadingManager)
-const texture = textureLoader.load('./static/image.jpeg')
-texture.colorSpace = THREE.SRGBColorSpace
-texture.repeat.x = 2
-texture.repeat.y = 3
-texture.wrapS = THREE.MirroredRepeatWrapping
-texture.wrapT = THREE.MirroredRepeatWrapping
-texture.offset.x = 0.5
-texture.offset.y = 0.5
-
-const gui = new GUI({ width: 300, title: 'Nice debug UI', closeFolders: true })
-const debug = { color: 0xff0000, subdivision: 2 }
 const canvas = document.querySelector('.webgl')
+const fontLoader = new FontLoader()
 const scene = new THREE.Scene()
-const material = new THREE.MeshNormalMaterial()
-material.side = THREE.DoubleSide
-material.flatShading = true
-const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material)
-sphere.position.x = -1.5
-const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material)
-const torus = new THREE.Mesh(new THREE.TorusGeometry(0.3, 0.2, 16, 32), material)
-torus.position.x = 1.5
-
-scene.add(sphere, plane, torus)
-
-const cubeTweaks = gui.addFolder('Cube')
-cubeTweaks.add(material, 'wireframe')
-cubeTweaks.addColor(debug, 'color').onChange(() => {
-  material.color.set(debug.color)
-})
-cubeTweaks.add(debug, 'spin')
-
-const axesHelper = new THREE.AxesHelper(2)
-scene.add(axesHelper)
-
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight
 }
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 1000)
-camera.position.z = 3
-scene.add(camera)
-
 const renderer = new THREE.WebGLRenderer({ canvas })
-renderer.setSize(sizes.width, sizes.height)
-
 const cursor = {
   x: 0,
   y: 0
 }
-
 const controls = new OrbitControls(camera, canvas)
+const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('./static/textures/matcaps/8.png')
+const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+const donuts = []
+
+for (let i = 0; i < 100; i++) {
+  const donut = new THREE.Mesh(donutGeometry, material)
+  donuts.push(donut)
+  const scale = Math.random()
+  donut.position.x = (Math.random() - 0.5) * 10
+  donut.position.y = (Math.random() - 0.5) * 10
+  donut.position.z = (Math.random() - 0.5) * 10
+  donut.rotation.x = Math.random() * Math.PI
+  donut.rotation.y = Math.random() * Math.PI
+  donut.scale.set(scale, scale, scale)
+  scene.add(donut)
+}
+
+matcapTexture.colorSpace = THREE.SRGBColorSpace
+fontLoader.load('./static/typeface/helvetiker_regular.typeface.json', (font) => {
+  const textGeometry = new TextGeometry('Hello ncnthien', {
+    font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 5
+  })
+  textGeometry.center()
+  const text = new THREE.Mesh(textGeometry, material)
+  scene.add(text)
+})
+
+camera.position.z = 3
+scene.add(camera)
+renderer.setSize(sizes.width, sizes.height)
 controls.enableDamping = true
 
 window.addEventListener('mousemove', (event) => {
@@ -84,41 +73,10 @@ window.addEventListener('resize', () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-window.addEventListener('dblclick', () => {
-  const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
-
-  if (!fullscreenElement) {
-    if (canvas.requestFullscreen) {
-      canvas.requestFullscreen()
-    } else if (canvas.webkitRequestFullscreen) {
-      canvas.webkitRequestFullscreen()
-    }
-  } else {
-    if (document.exitFullscreen) {
-      document.exitFullscreen()
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen()
-    }
-  }
-})
-
-window.addEventListener('keydown', (event) => {
-  if (event.key === 'h') {
-    gui.show(gui._hidden)
-  }
-})
-
 const clock = new THREE.Clock()
 
-function tick() {
+function tick(time = 1) {
   const elapsedTime = clock.getElapsedTime()
-  sphere.rotation.y = 0.1 * elapsedTime
-  plane.rotation.y = 0.1 * elapsedTime
-  torus.rotation.y = 0.1 * elapsedTime
-
-  sphere.rotation.x = - 0.15 * elapsedTime
-  plane.rotation.x = - 0.15 * elapsedTime
-  torus.rotation.x = - 0.15 * elapsedTime
 
   renderer.render(scene, camera)
   controls.update()
